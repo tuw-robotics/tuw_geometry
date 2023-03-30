04a221b61b93eff30ed9e9d29d687a1118a44b75
Markus Bader <markus.bader@mx-robotics.com>
2023-03-30 19:01:23 +0200

    connand2d files added

---
+++ include/tuw_geometry/command2d.hpp
@@ -0,0 +1,62 @@
+#ifndef TUW_GEOMETRY__COMMAND2D_HPP
+#define TUW_GEOMETRY__COMMAND2D_HPP
+
+#include <memory>
+#include <opencv2/core/core.hpp>
+
+namespace tuw
+{
+class Command2D;   /// Prototype
+using Command2DPtr = std::shared_ptr<Command2D>;
+using Command2DConstPtr = std::shared_ptr<Command2D const>;
+
+/**
+ * class to handle motion control commands
+ **/
+class Command2D : public cv::Vec<double, 2>
+{
+public:
+  /// constructor
+  Command2D();
+  /// constructor
+  Command2D(double v, double w);
+  /// copy constructor
+  Command2D(const Command2D & o);
+
+  /**
+     * Stream extraction
+     * @param os outputstream
+     * @param o object
+     * @return stream
+     **/
+  friend std::ostream & operator<<(std::ostream & os, const Command2D & o);
+
+  /**
+     * @return linear velocity
+     **/
+  double & v();
+  /**
+     * @return linear velocity
+     **/
+  const double & v() const;
+
+  /**
+     * @return angular velocity
+     **/
+  double & w();
+
+  /**
+     * @return angular velocity
+     **/
+  const double & w() const;
+
+  /**
+     * @param v linear velocity
+     * @param w angular velocity
+     **/
+  void set(double v, double w);
+};
+}  // namespace tuw
+
+std::ostream & operator<<(std::ostream & os, const tuw::Command2D & o);
+#endif  //TUW_GEOMETRY__COMMAND2D_HPP
---
+++ src/tuw_geometry/command2d.cpp
@@ -0,0 +1,28 @@
+#include "tuw_geometry/command2d.hpp"
+using namespace tuw;
+
+Command2D::Command2D()
+: cv::Vec<double, 2>(0, 0) {}
+
+Command2D::Command2D(double v, double w)
+: cv::Vec<double, 2>(v, w) {}
+
+Command2D::Command2D(const Command2D & o)
+: cv::Vec<double, 2>(o) {}
+
+double & Command2D::v() {return this->val[0];}
+
+const double & Command2D::v() const {return this->val[0];}
+
+double & Command2D::w() {return this->val[1];}
+
+const double & Command2D::w() const {return this->val[1];}
+
+void Command2D::set(double v, double w) {this->val[0] = v, this->val[1] = w;}
+
+
+std::ostream & operator<<(std::ostream & os, const tuw::Command2D & o)
+{
+  os << "[" << o.v() << ", " << o.w() << "]";
+  return os;
+}
