08a6ca9dfefb20595381aa448c4015cf03535711
Markus Bader <markus.bader@mx-robotics.com>
2023-04-18 10:10:58 +0200

    uncrustified

--- include/tuw_geometry/stamp.hpp
+++ include/tuw_geometry/stamp.hpp
@@ -3,27 +3,27 @@
 
 namespace tuw
 {
-  template <class T>
-  class Stamp
+template<class T>
+class Stamp
+{
+public:
+  std::chrono::microseconds stamp;
+  tuw::Tf2D tf;
+  T z;
+  friend bool operator<(const Stamp<T> & l, const Stamp<T> & r)
   {
-    public:
-    std::chrono::microseconds stamp;
-    tuw::Tf2D tf;
-    T z;
-    friend bool operator<(const Stamp<T> &l, const Stamp<T> &r)
-    {
-      return l.stamp < r.stamp;
-    }
-    friend bool operator>(const Stamp<T> &l, const Stamp<T> &r)
-    {
-      return l.stamp > r.stamp;
-    }
-    friend bool operator==(const Stamp<T> &l, const Stamp<T> &r)
-    {
-      return l.stamp == r.stamp;
-    }
-  };
-  template <class T> using StampPtr = std::shared_ptr<Stamp <T> >;
-  template <class T> using StampConstPtr = std::shared_ptr<Stamp<T> const>;
+    return l.stamp < r.stamp;
+  }
+  friend bool operator>(const Stamp<T> & l, const Stamp<T> & r)
+  {
+    return l.stamp > r.stamp;
+  }
+  friend bool operator==(const Stamp<T> & l, const Stamp<T> & r)
+  {
+    return l.stamp == r.stamp;
+  }
+};
+template<class T> using StampPtr = std::shared_ptr<Stamp<T>>;
+template<class T> using StampConstPtr = std::shared_ptr<Stamp<T> const>;
 }
 #endif // TUW_GEOMETRY__STAMP_HPP
